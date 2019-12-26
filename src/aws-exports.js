4fc8026101cd24e1dc39c51d51455426ada0013a
g-awa <gee.awa@gmail.com>
2019-12-26 12:44:27 +0900

    Update aws-exports.js

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -5,7 +5,7 @@ const awsmobile =  {
     "aws_appsync_graphqlEndpoint": "https://b5ca4ozqi5hwtp6hl6al72amle.appsync-api.ap-northeast-1.amazonaws.com/graphql",
     "aws_appsync_region": "ap-northeast-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-ncdo7mcuvrfi3khtwrtspwxbaq"
+    "aws_appsync_apiKey": "da2-livtx7p2rjacvo3ygfssqqo5fq"
 };
 
 
